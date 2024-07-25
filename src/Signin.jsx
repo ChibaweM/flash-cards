df9769a51135b3011891ffe0bc4dfa6031e64ad9
ChibaweM <chibawemkwenya@gmail.com>
2024-07-25 10:33:00 +0200

    init 2

--- src/Signin.jsx
+++ src/Signin.jsx
@@ -3,7 +3,14 @@ import React from 'react'
 function Signin() {
   return (
     <div>
-      <p>hello baby</p>
+      
+    <p>hello baby</p>
+    <p>hello baby</p>
+    <p>hello baby</p>
+    <p>hello baby</p>
+    <p>hello baby</p>
+    <p>hello baby</p>
+
     </div>
   )
 }
