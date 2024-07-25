40663d2e84956fadde8d6cbd730a3a3151549702
ChibaweM <chibawemkwenya@gmail.com>
2024-07-25 11:07:05 +0200

    adding tailwind

--- src/Signin.jsx
+++ src/Signin.jsx
@@ -2,15 +2,11 @@ import React from 'react'
 
 function Signin() {
   return (
-    <div>
-      
-    <p>hello baby</p>
-    <p>hello baby</p>
-    <p>hello baby</p>
-    <p>hello baby</p>
-    <p>hello baby</p>
-    <p>hello baby</p>
-
+    <div className="min-h-screen bg-gray-600 flex items-center justify-center">
+      <div className="bg-white p-6 rounded-lg shadow-lg">
+        <h1 className="text-2xl mb-4">Flashcards App</h1>
+        <p className="text-gray-700">Welcome to your flashcards app!</p>
+      </div>
     </div>
   )
 }
