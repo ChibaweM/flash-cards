94560db2974c5ff40eed370bf062fa1d406dd53f
ChibaweM <chibawemkwenya@gmail.com>
2024-09-02 19:19:52 +0200

    commit of the day

--- src/components/Signin.jsx
+++ src/components/Signin.jsx
@@ -1,41 +1,66 @@
 // src/components/SignIn.js
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 function Signin() {
+  const [email, setEmail] = useState();
+  const [conPassword, setconPassword] = useState();
+  const [password, setPassword] = useState();
+
   return (
     <div className="min-h-screen flex items-center justify-center bg-white">
       <div className="w-full max-w-md p-8 space-y-6 bg-white rounded-lg ">
-        <h2 className="text-center text-2xl font-bold text-olive">Welcome to FlashBuddy</h2>
+        <h2 className="text-center text-2xl font-bold text-olive">
+          Welcome to Studybuddy
+        </h2>
         <h6 className="text-center text-leather">Sign In</h6>
         <form className="mt-8 space-y-6">
           <div className="rounded-md shadow-sm -space-y-px">
             <div>
-              <label htmlFor="email-address" className="sr-only">Email address</label>
-              <input id="email-address" name="email" type="email" autoComplete="email" required
+              <label htmlFor="username" className="sr-only">
+                Username
+              </label>
+              <input
+                id="username"
+                name="username"
+                type="test"
+                autoComplete="username"
+                required
                 className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-olive focus:border-olive focus:z-10 sm:text-sm"
-                placeholder="Email address" />
+                placeholder="Username"
+              />
             </div>
             <div>
-              <label htmlFor="password" className="sr-only">Password</label>
-              <input id="password" name="password" type="password" autoComplete="current-password" required
+              <label htmlFor="password" className="sr-only">
+                Password
+              </label>
+              <input
+                id="password"
+                name="password"
+                type="password"
+                autoComplete="current-password"
+                required
                 className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-olive focus:border-olive focus:z-10 sm:text-sm"
-                placeholder="Password" />
+                placeholder="Password"
+              />
             </div>
           </div>
 
           <div>
-            <button type="submit"
-              className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-olive hover:bg-leather focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-olive">
+            <button
+              type="submit"
+              className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-olive hover:bg-leather focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-olive"
+            >
               Sign In
             </button>
           </div>
         </form>
         <div className="relative w-full flex justify-center">
-          <p>Dont have an account?</p>
+          <p>
           <Link to="/signup" className=" text-leather mr-4">
-            Register
+            Register Account
           </Link>
+          </p>
         </div>
       </div>
     </div>
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,18 +1,80 @@
 // src/components/SignUp.js
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom";
 
 function Signup() {
+  const userRef = useRef();
+  const errRef = useRef();
+
+  //to capture the details from the form
+  const [username, setUsername] = useState("");
+  const [validName, setValidName] = useState(false);
+  const [nameFocus, setNameFocus] = useState(false);
+
+  const [email, setEmail] = useState("");
+  const [validEmail, setValidEmail] = useState(false);
+  const [emailFocus, setEmailFocus] = useState(false);
+
+  const [pwd, setPwd] = useState("");
+  const [validPwd, setValidPwd] = useState(false);
+  const [pwdFocus, setPwdFocus] = useState(false);
+
+  const [conPwd, setConPwd] = useState("");
+  const [validConPwd, setValidConPwd] = useState(false);
+  const [conPwdFocus, setConPwdFocus] = useState(false);
+
+  //part to help with error messages
+  const [errMsg, setErrMsg] = useState("");
+  const [success, setSuccess] = useState(false);
+
+
+ /*  //helpers to capture the data from the form
+  useEffect(() => {
+    userRef.current.focus();
+  }, []);
+
+  useEffect(() => {
+    setValidName(USER_REGEX.test(username));
+  }, [username]);
+
+  useEffect(() => {
+    setValidEmail(USER_REGEX.test(email));
+  }, [username]);
+
+  useEffect(() => {
+    setValidPwd(PWD_REGEX.test(pwd));
+    setValidConPwd(pwd === conPwd);
+  }, [pwd, conPwd]);
+
+  useEffect(() => {
+    setErrMsg("");
+  }, [username, pwd, conPwd]); */
+
   return (
     <div className="min-h-screen flex items-center justify-center bg-white">
       <div className="w-full max-w-md p-8 space-y-6 bg-white">
-        <h2 className="text-center text-2xl font-bold text-olive">Wlecome to FlashBuddy</h2>
-        <h4 className="text-center text-olive"> Sign Up</h4>
+        <h2 className="text-center text-2xl font-bold text-olive">
+          welcome to studybuddy
+        </h2>
+        <h4 className="text-center text-olive"> sign up</h4>
         <form onSubmit={""} className="mt-8 space-y-6">
           <div className="rounded-md shadow-sm -space-y-px">
             <div>
+              <label htmlFor="username" className="sr-only">
+                username
+              </label>
+              <input
+                id="username"
+                name="username"
+                autoComplete="current-password"
+                required
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-olive focus:border-olive focus:z-10 sm:text-sm"
+                placeholder="username"
+              />
+            </div>
+            <div>
               <label htmlFor="email-address" className="sr-only">
-                Email address
+                email address
               </label>
               <input
                 id="email-address"
@@ -20,13 +82,13 @@ function Signup() {
                 type="email"
                 autoComplete="email"
                 required
-                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-olive focus:border-olive focus:z-10 sm:text-sm"
-                placeholder="Email address"
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900  focus:outline-none focus:ring-olive focus:border-olive focus:z-10 sm:text-sm"
+                placeholder="email address"
               />
             </div>
             <div>
               <label htmlFor="password" className="sr-only">
-                Passwords
+                passwords
               </label>
               <input
                 id="password"
@@ -35,12 +97,12 @@ function Signup() {
                 autoComplete="current-password"
                 required
                 className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900  focus:outline-none focus:ring-olive focus:border-olive focus:z-10 sm:text-sm"
-                placeholder="Password"
+                placeholder="password"
               />
             </div>
             <div>
               <label htmlFor="confirmpassword" className="sr-only">
-                confirmpassword
+                confirm password
               </label>
               <input
                 id="confirmpassword"
@@ -49,7 +111,7 @@ function Signup() {
                 autoComplete="current-password"
                 required
                 className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-olive focus:border-olive focus:z-10 sm:text-sm"
-                placeholder="Confirm Password"
+                placeholder="confirm password"
               />
             </div>
           </div>
@@ -58,15 +120,16 @@ function Signup() {
               type="submit"
               className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-olive hover:bg-leather focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-olive"
             >
-              Register
+              register
             </button>
           </div>
         </form>
         <div className="relative w-full flex justify-center">
-          <p>Already have an account?</p>
-          <Link to="/signin" className=" text-leather mr-4">
-            Sign In
-          </Link>
+          <p>
+            <Link to="/signin" className=" text-leather mr-4">
+              sign in with existing account
+            </Link>
+          </p>
         </div>
       </div>
     </div>
